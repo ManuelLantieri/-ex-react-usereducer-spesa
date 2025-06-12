2cbef59b95c5b003dd48163f3151303bfe9d97e9
manuel lantieri <manuel.lantieri02@gmail.com>
2025-06-12 11:19:54 +0200

    milestone 3

--- ex-react-usereducer-spesa/src/App.jsx
+++ ex-react-usereducer-spesa/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 function App() {
   const products = [
@@ -9,45 +9,62 @@ function App() {
   ];
 
   const [addedProducts, setAddedProducts] = useState([]);
-  const addToCart = (product) => {
-    const isProductAlreadyAdded = addedProducts.some(
-      (p) => p.name === product.name
+
+  const uptadeProductQuantity = (name, quantity) => {
+    setAddedProducts((curr) =>
+      curr.map((p) => (p.name === name ? { ...p, quantity } : p))
     );
-    if (isProductAlreadyAdded) {
+  };
+
+  const addToCart = (product) => {
+    const addedProducts = addedProducts.find((p) => p.name === product.name);
+    if (addedProducts) {
+      uptadeProductQuantity(addedProduct.name, addedProduct.quantity + 1);
       return;
     }
     setAddedProducts((curr) => [...curr, { product, quantity: 1 }]);
   };
 
-  return (
-    <>
-      <h1>Carrello</h1>
-      <ul>
-        {products.map((p, i) => (
-          <li key={i}>
-            <p>
-              {p.name} - {p.price.toFixed(2)}€
-            </p>
-            <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
-          </li>
-        ))}
-      </ul>
-      {addedProducts.length > 0 && (
-        <>
-          <h2>Carrello</h2>
-          <ul>
-            {addedProducts.map((p, i) => (
-              <li key={i}>
-                <p>
-                  {p.name} - {p.price.toFixed(2)}€ x {p.quantity}
-                </p>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-    </>
-  );
+  const removeFromCart = (product) => {
+    setAddedProducts((curr) => curr.filter((p) => p.name !== product.name));
+
+    const totalToPay = addedProducts.reduce((acc, p) => {
+      acc + p.price * p.quantity;
+    }, 0);
+    return (
+      <>
+        <h1>Carrello</h1>
+        <ul>
+          {products.map((p, i) => (
+            <li key={i}>
+              <p>
+                {p.name} - {p.price.toFixed(2)}€
+              </p>
+              <button onClick={() => addToCart(p)}>aggiungi al carrello</button>
+            </li>
+          ))}
+        </ul>
+        {addedProducts.length > 0 && (
+          <>
+            <h2>Carrello</h2>
+            <ul>
+              {addedProducts.map((p, i) => (
+                <li key={i}>
+                  <p>
+                    {p.name} - {p.price.toFixed(2)}€ x {p.quantity}
+                  </p>
+                  <button onClick={() => removeFromCart(p)}>
+                    Rimuovi dal carrello
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <h3>totale {totalToPay.toFixed(2)}</h3>
+          </>
+        )}
+      </>
+    );
+  };
 }
 
 export default App;
