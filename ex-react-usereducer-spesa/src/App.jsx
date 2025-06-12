9aca9fe47222871633206e1f4abca66d702f80da
manuel lantieri <manuel.lantieri02@gmail.com>
2025-06-12 11:18:07 +0200

    milestone2

--- ex-react-usereducer-spesa/src/App.jsx
+++ ex-react-usereducer-spesa/src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 function App() {
   const products = [
     { name: "Mela", price: 0.5 },
@@ -5,6 +7,17 @@ function App() {
     { name: "Latte", price: 1.0 },
     { name: "Pasta", price: 0.7 },
   ];
+
+  const [addedProducts, setAddedProducts] = useState([]);
+  const addToCart = (product) => {
+    const isProductAlreadyAdded = addedProducts.some(
+      (p) => p.name === product.name
+    );
+    if (isProductAlreadyAdded) {
+      return;
+    }
+    setAddedProducts((curr) => [...curr, { product, quantity: 1 }]);
+  };
 
   return (
     <>
@@ -15,9 +28,24 @@ function App() {
             <p>
               {p.name} - {p.price.toFixed(2)}€
             </p>
+            <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
+      {addedProducts.length > 0 && (
+        <>
+          <h2>Carrello</h2>
+          <ul>
+            {addedProducts.map((p, i) => (
+              <li key={i}>
+                <p>
+                  {p.name} - {p.price.toFixed(2)}€ x {p.quantity}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
